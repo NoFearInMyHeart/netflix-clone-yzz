8910202be02894c1a1a04dae360a53c8cdff15f5
zYz <zhuyueze>
2022-11-10 21:06:41 +0800

    init

---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/layout';
+
+export default function Login() {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <main>login</main>
+    </Layout>
+  );
+}
